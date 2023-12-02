a7c36442a5d0dd97ee179b945b62c5662bf2e757
grigaut <gaetanrigaut.s@gmail.com>
2023-12-02 16:04:31 +0100

    Add preprocessing tools

---
+++ src/bramm_data_analysis/preprocessing/__init__.py
@@ -0,0 +1,6 @@
+"""Data Preprocessing Tools."""
+
+from bramm_data_analysis.preprocessing.moss import MossPreprocessor
+from bramm_data_analysis.preprocessing.rmqs import RMQSPreprocessor
+
+__all__ = ["MossPreprocessor", "RMQSPreprocessor"]
