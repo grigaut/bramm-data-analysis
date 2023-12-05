02de4bb34de255999fcf054e2c95c8be63e7adcd
grigaut <gaetanrigaut.s@gmail.com>
2023-12-05 12:06:00 +0100

    Add Scaler Object

--- src/bramm_data_analysis/preprocessing/__init__.py
+++ src/bramm_data_analysis/preprocessing/__init__.py
@@ -2,5 +2,6 @@
 
 from bramm_data_analysis.preprocessing.moss import MossPreprocessor
 from bramm_data_analysis.preprocessing.rmqs import RMQSPreprocessor
+from bramm_data_analysis.preprocessing.scaler import Scaler
 
-__all__ = ["MossPreprocessor", "RMQSPreprocessor"]
+__all__ = ["MossPreprocessor", "RMQSPreprocessor", "Scaler"]
