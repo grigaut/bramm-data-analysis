9d02110040d6d38601539971c39461f67f3ac84a
grigaut <gaetanrigaut.s@gmail.com>
2023-12-27 11:47:50 +0100

    Improve core loading functions docstrings

--- src/bramm_data_analysis/loaders/core.py
+++ src/bramm_data_analysis/loaders/core.py
@@ -7,10 +7,32 @@ from bramm_data_analysis.loaders.rmqs import RMQSLoader
 
 
 def from_moss_csv(data_path: Path) -> MossLoader:
-    """Load DataBase from Moss CSV file."""
+    """Load DataBase from Moss CSV file.
+
+    Parameters
+    ----------
+    data_path : Path
+        Path to the source file containing the data.
+
+    Returns
+    -------
+    RMQSLoader
+    Loader for Moss Data.
+    """
     return MossLoader(source=data_path)
 
 
 def from_rmqs_csv(data_path: Path) -> RMQSLoader:
-    """Load DataBase from RMQS CSV file."""
+    """Load DataBase from RMQS CSV file.
+
+    Parameters
+    ----------
+    data_path : Path
+        Path to the source file containing the data.
+
+    Returns
+    -------
+    RMQSLoader
+        Loader for RMQS Data.
+    """
     return RMQSLoader(source=data_path)
